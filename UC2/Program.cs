932f1c166fa5007f9c2ae56eef44605b1e0d685b
SoumyaD27 <soumyad233@gmail.com>
2022-12-02 18:52:14 +0530

    Calculate Daily Employee Wage

---
+++ UC2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isFullTime = 1;
+            int empRatePerHrs = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if(empCheck==isFullTime)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * empRatePerHrs;
+            Console.WriteLine("Emp Wage: {0}", empWage);
+        }
+    }
+}
