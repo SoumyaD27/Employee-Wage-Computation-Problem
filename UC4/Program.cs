3c4dc12d70660d144cd9f08f2a1f15dbcc13ef91
SoumyaD27 <soumyad233@gmail.com>
2022-12-02 19:16:58 +0530

    Employee Wage using Switch

---
+++ UC4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace UC4
+{
+    class Program
+    {
+        public const int isPartTime = 1;
+        public const int isFullTime = 2;
+        public const int empRatePerHrs = 20;
+        static void Main(string[] args)
+        {
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch(empCheck)
+            {
+                case isPartTime:
+                    empHrs = 4;
+                    break;
+
+                case isFullTime:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+
+            }
+            empWage = empHrs * empRatePerHrs;
+            Console.WriteLine("Emp Wage: {0}", empWage);
+        }
+    }
+}
