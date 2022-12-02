bdcf2109bfc9d36cb9f94b8e8340aae53020b385
SoumyaD27 <soumyad233@gmail.com>
2022-12-02 20:12:40 +0530

    Wage till a condition of total working hrs

---
+++ UC6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace UC6
+{
+    class Program
+    {
+        public const int isPartTime = 1;
+        public const int isFullTime = 2;
+        public const int empRatePerHrs = 20;
+        public const int workingDays = 2;
+        public const int maxHrsInMonth = 10;
+        static void Main(string[] args)
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while(totalEmpHrs<=maxHrsInMonth && totalWorkingDays<workingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch(empCheck)
+                {
+                    case isPartTime:
+                        empHrs = 4;
+                        break;
+                    case isFullTime:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days:" + totalWorkingDays + "Emp Hrs: " + empHrs);
+
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHrs;
+            Console.WriteLine("Total Emp Wage: " + totalEmpWage);
+        }
+    }
+}
